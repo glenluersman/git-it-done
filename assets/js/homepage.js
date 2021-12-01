3bb5c3f1a294e21a7cdb8ccbe4e348f73cf9fb7a
glenluersman <glen.luersman@gmail.com>
2021-11-30 21:26:12 -0500

    added language buttons

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,6 +2,7 @@ var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
+var languageButtonsEl = document.querySelector("#language-buttons");
 
 var getUserRepos = function(user) {
     // format the github api url
@@ -88,6 +89,35 @@ var getUserRepos = function(user) {
       console.log(searchTerm);
   };
 
+  var getFeaturedRepos = function(language) {
+    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+
+    fetch(apiUrl).then(function(response) {
+      if (response.ok) {
+        response.json().then(function(data) {
+          displayRepos(data.items, language);
+          console.log(response);
+        })
+      } else {
+        alert("Error: GitHub User Not Found");
+      }
+    });
+  };
+
+  var buttonClickHandler = function(event) {
+    var language = event.target.getAttribute("data-language");
+    
+    if (language) {
+      getFeaturedRepos(language);
+
+      //clear old content
+      repoContainerEl.textContent = "";
+    }
+  }
+
+  languageButtonsEl.addEventListener("click", buttonClickHandler);
+
   userFormEl.addEventListener("submit", formSubmitHandler);
 
   getUserRepos()
+  
